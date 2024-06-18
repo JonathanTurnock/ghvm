c1cd6e7c53f9ed544cedea12416b0a4eed366196
🐸 <dice-a6db@outlook.com>
2024-06-18 20:41:51 +0100

    chore: remote constants file

--- src/constants.ts
+++
@@ -1,4 +0,0 @@
-export const PROGRAM_NAME = "ghvm";
-export const PROGRAM_DESCRIPTION =
-  "A tool for managing GitHub releases and installing them as apps";
-export const PROGRAM_VERSION = "0.1.0";
